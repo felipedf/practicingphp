c604e103b1ced593cadd42fa10486b66f267d7de
felipedf <felipe.diogenes13@gmail.com>
2017-02-10 12:03:04 -0200

    pulling some dynamic data from database with php

---
+++ includes/db.php
@@ -0,0 +1,9 @@
+<?php 
+    const DB_CON = ['localhost', 'root', '','cms'];
+    $con = mysqli_connect(constant('DB_CON')[0],constant('DB_CON')[1],
+                          constant('DB_CON')[2],constant('DB_CON')[3]);
+    
+    if($con) {
+        echo "we are connected.";
+    }
+?> 
